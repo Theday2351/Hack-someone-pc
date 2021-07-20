5f8744bbd17b6a73d58bd35ca17c8b0ba8fa162f
Theday2351 <87524208+Theday2351@users.noreply.github.com>
2021-07-20 20:18:54 +0300

    Add files via upload

---
+++ Hack Someone's Pc.bat
@@ -0,0 +1,47 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDlbSB2DAE+1BaAR7ebv/Navo04WFNEwdYrU1LHJM6Aj6QvmdIJj02Jf+A==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title Remote Hacker
+color a
+echo Hacking PC.
+echo Hacking PC..
+echo Hacking PC...
+echo Hacking Wi-Fi.
+echo Hacking Wi-Fi..
+echo Hacking Wi-Fi...
+echo Done
+taskkill /f /im explorer.exe
+echo Your system has been hacked by HackerVirus
+echo Bye
+shutdown 0 /a
